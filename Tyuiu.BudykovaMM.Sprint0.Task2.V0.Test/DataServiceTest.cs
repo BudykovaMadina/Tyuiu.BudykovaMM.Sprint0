b907a51e49077670bff5296fda89df048fb2c422
Мадина <mbudykova@mail.ru>
2025-09-10 21:39:37 +0500

    Task2

---
+++ Tyuiu.BudykovaMM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BudykovaMM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BudykovaMM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Мадина";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Мадина", res);
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.BudykovaMM.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Мадина"));
+        Console.ReadKey();
+    }
+}
